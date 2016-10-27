bdd35b3e494bbe2b5bfa4b36e0b481e208a9116a
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-10-27 13:57:29 -0400

    Done

--- ClosuresAreGreat/SortingStuff.swift
+++ ClosuresAreGreat/SortingStuff.swift
@@ -1,13 +1,4 @@
-//
-//  SortingStuff.swift
-//  ClosuresAreGreat
-//
-//  Created by Jim Campagno on 10/25/16.
-//  Copyright © 2016 Gamesmith, LLC. All rights reserved.
-//
-
 import Foundation
-
 
 struct ToyBin {
     
@@ -15,12 +6,50 @@ struct ToyBin {
     var books: [Book] = []
     var bowlingPins: [BowlingPin] = []
     var musicCDs: [MusicCD] = []
-    
-    // TODO: Implement all of the sort functions (lets organize this toy bin!)
-    
  
-}
+    // struct - mutating func!
+    mutating func sortShips() {
+        ships = ships.sorted(by: { ship1, ship2 in ship1.age > ship2.age })
+        // sorted takes a closure as an argument
+        
+        //"Titanic" should be at the front of the list
+        for (index, ship) in ships.enumerated() {
+            if ship.name == "Titanic" {
+                ships.remove(at: index)
+                ships.insert(ship, at: 0)
+            }
+        }
+    }
+    
+    mutating func sortBooks() {
+        books = books.sorted(by: {return $0.name < $1.name})
+    }
 
+    
+    mutating func sortBowlingPins() {
+        bowlingPins = bowlingPins.sorted(by: {bowlingPin1, bowlingPin2 in return bowlingPin1.color.rawValue < bowlingPin2.color.rawValue })
+    }
+    
+    mutating func sortMusicCDs() {
+        musicCDs = musicCDs.sorted(by: { musicCd1, musicCd2 in musicCd1.name < musicCd2.name })
+        for (index, musicCd) in musicCDs.enumerated() {
+            if musicCd.name == "Drake" {
+                musicCDs.remove(at: index)
+                musicCDs.insert(musicCd, at: 0)
+            }
+        }
+    }
+    
+    mutating func changeColorOfAllPins(to color:Color) {
+        bowlingPins = bowlingPins.map{ pin in
+        var copy = pin
+        copy.changeColor(to: color)
+        return copy
+        }
+        
+    }
+    
+}
 
 struct Ship {
     var name: String
@@ -39,6 +68,10 @@ struct BowlingPin {
     var name: String
     var film: String
     var color: Color
+    
+    mutating func changeColor(to newColor:Color){
+        color = newColor
+    }
 }
 
 
@@ -53,3 +86,4 @@ struct MusicCD {
     var year: Int
     var songs: [String]
 }
+
